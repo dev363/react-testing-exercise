4c9e41a0fbe3bdf40f96584555d3376fc737d877
dev363 <satn363@gmail.com>
2023-10-16 11:00:00 +0530

    Jest Basic sum testing

---
+++ src/components/sum/Sum.test.tsx
@@ -0,0 +1,13 @@
+import Sum from "./Sum";
+
+describe("Sum", () => {
+  test("Numbee a:1 and b:2  sum = 3", () => {
+    const result = Sum(1, 2);
+    expect(result).toBe(3);
+  });
+
+  test("When a and B are string", () => {
+    const result = Sum("1", "2");
+    expect(result).toBe(3);
+  });
+});
